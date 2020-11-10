9b04095c8e079bcb69faf208a70f793ccaa08327
Troy Astrope <troy.astrope@shaw.ca>
2020-11-10 13:19:17 -0600

    Initial commit of my first Unity project. Just messing around and trying it out.

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    void Start()
+    {
+        target = Waypoints.waypoints[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.1f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+
+        if (waypointIndex >= Waypoints.waypoints.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        waypointIndex++;
+        target = Waypoints.waypoints[waypointIndex];
+    }
+
+}
---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 3f;
+
+    public Text waveCountdownText;
+    public Text waveNumberText;
+
+    public int waveIndex = 0;
+
+    void Update()
+    {
+        if (countdown <= 0)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+
+        waveCountdownText.text = Mathf.Ceil(countdown).ToString();
+        waveNumberText.text = "Wave " + (waveIndex).ToString();
+
+    }
+
+    IEnumerator SpawnWave ()
+    {
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
